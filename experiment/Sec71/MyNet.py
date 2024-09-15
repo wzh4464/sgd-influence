11ed83c1ee8c89a946de6d7fb10971ba7daa6262
zihan_cityu <32484940+wzh4464@users.noreply.github.com>
2024-09-15 16:10:54 +0800

    feat: CNN module

--- experiment/Sec71/MyNet.py
+++ experiment/Sec71/MyNet.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
@@ -50,8 +51,8 @@ class DNN(nn.Module):
         super(DNN, self).__init__()
         self.layers = nn.Sequential(
             nn.Linear(input_dim, m[0]),
-            nn.ReLU(),
-            nn.Linear(m[0], m[1]),
+            # nn.ReLU(),
+            # nn.Linear(m[0], m[1]),
             nn.ReLU(),
             nn.Linear(m[1], 1),
         )
@@ -108,3 +109,23 @@ class DNN(nn.Module):
                 print(f"Difference in {name}:")
                 print(diff_tensor)
                 print()
+
+
+class CifarCNN(nn.Module):
+    def __init__(self):
+        super(CifarCNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 32, 3, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
+        self.conv3 = nn.Conv2d(64, 64, 3, padding=1)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(64 * 4 * 4, 64)
+        self.fc2 = nn.Linear(64, 1)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
+        x = x.view(-1, 64 * 4 * 4)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
