51539ae2e404353e1162ebff73c60e9ead6ea387
zihan_cityu <32484940+wzh4464@users.noreply.github.com>
2024-09-25 10:03:56 +0800

    adult dnn config

--- experiment/Sec71/config.py
+++ experiment/Sec71/config.py
@@ -3,7 +3,7 @@
 # Created Date: Friday, September 20th 2024
 # Author: Zihan
 # -----
-# Last Modified: Tuesday, 24th September 2024 8:54:19 am
+# Last Modified: Wednesday, 25th September 2024 10:01:13 am
 # Modified By: the developer formerly known as Zihan at <wzh4464@gmail.com>
 # -----
 # HISTORY:
@@ -84,7 +84,7 @@ DATASET_NETWORK_CONFIG = {
     ("adult", "dnn"): {
         "num_epoch": 21,
         "batch_size": 64,
-        "lr": 0.01,
+        "lr": 0.3,
         "decay": True,
         "n_tr": 256,
         "n_val": 256,
